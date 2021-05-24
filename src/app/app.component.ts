ff739fe69526d24b573b68d8227176fc3152a4da
skorpionis <skorp9393@mail.ru>
2021-05-25 00:15:34 +0300

    1 Задание - html Шаблон

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,30 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '' +
-    '<div style="..." class="content"> ' +
-    '<h1> welcome to {{title}}!</h1> ' +
-    '<span style="...">{{title}} app is runnin!</span> ' +
-    '<img width="300" alt="Angular Logo" src="data:"> ' +
-    '</div>' +
-    '<h2>Paru linkov dlia smeha: </h2>' +
-    '<ul>' +
-      '<li>' +
-        '<h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial"></a></h2>' +
-      '</li>' +
-    '<li>' +
-        '<h2><a target="_blank" rel="noopener" href="https://angular.io/cli"></a></h2>' +
-      '</li>' +
-    '<li>' +
-        '<h2><a target="_blank" rel="noopener" href="https://blog.angular.io/"></a></h2>' +
-      '</li>' +
-     '</ul>',
+  template: `
+      <div style="text-align: center">
+          <h2>Paru linkov dlia smeha: </h2>
+          <ul>
+              <li>
+                  <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of tutorial</a></h2> 
+              </li>
+              
+              <li> 
+                  <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2> 
+              </li>
+              <li> 
+                  <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2> 
+              </li>
+          </ul>
+      </div>
+      <div style="text-align: center" class="content"> 
+          <h1>
+              <div style="text-align: center;">Первый проект - {{title}}!</div>
+          </h1>
+          <div style="text-align: center;">{{title}} app is runnin!</div>
+          <img width="300" alt="Angular Logo" src="favicon.ico"> 
+      </div>`,
   styles: []
-
 })
 export class AppComponent {
   title = 'FirstAngularProject';
