3c54625e6d944a010a5071f24f755b11ee22c3ac
skorpionis <skorp9393@mail.ru>
2021-05-24 23:24:01 +0300

    Пробный Angular проект

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '' +
+    '<div style="..." class="content"> ' +
+    '<h1> welcome to {{title}}!</h1> ' +
+    '<span style="...">{{title}} app is runnin!</span> ' +
+    '<img width="300" alt="Angular Logo" src="data:"> ' +
+    '</div>' +
+    '<h2>Paru linkov dlia smeha: </h2>' +
+    '<ul>' +
+      '<li>' +
+        '<h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial"></a></h2>' +
+      '</li>' +
+    '<li>' +
+        '<h2><a target="_blank" rel="noopener" href="https://angular.io/cli"></a></h2>' +
+      '</li>' +
+    '<li>' +
+        '<h2><a target="_blank" rel="noopener" href="https://blog.angular.io/"></a></h2>' +
+      '</li>' +
+     '</ul>',
+  styles: []
+
 })
 export class AppComponent {
   title = 'FirstAngularProject';
