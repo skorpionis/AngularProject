87b3a4abef7b619c70be14db3264e0502a21e48f
skorpionis <skorp9393@mail.ru>
2021-05-25 11:37:46 +0300

    1 Задание - html Шаблон

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-      <div style="text-align: center">
-          <h2>Paru linkov dlia smeha: </h2>
-          <ul>
-              <li>
-                  <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of tutorial</a></h2> 
-              </li>
-              
-              <li> 
-                  <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2> 
-              </li>
-              <li> 
-                  <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2> 
-              </li>
-          </ul>
-      </div>
-      <div style="text-align: center" class="content"> 
-          <h1>
-              <div style="text-align: center;">Первый проект - {{title}}!</div>
-          </h1>
-          <div style="text-align: center;">{{title}} app is runnin!</div>
-          <img width="300" alt="Angular Logo" src="favicon.ico"> 
-      </div>`,
+  templateUrl: 'app.component.html',
   styles: []
 })
 export class AppComponent {
