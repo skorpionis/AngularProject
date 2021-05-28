c49849c825d7b663af7ba07b4004f0dc5fcb1c86
skorpionis <skorp9393@mail.ru>
2021-05-28 16:02:54 +0300

    Добавил отображение всех Tasks без фильтрации по категориям

--- src/app/data/TestData.ts
+++ src/app/data/TestData.ts
@@ -7,23 +7,23 @@ export class TestData {
 
   static categories: Category[] = [
     {id: 1, title: 'Работа'},
-    {id: 1, title: 'Семья'},
-    {id: 1, title: 'Учеба'},
-    {id: 1, title: 'Отдых'},
-    {id: 1, title: 'Спорт'},
-    {id: 1, title: 'Еда'},
-    {id: 1, title: 'Финансы'},
-    {id: 1, title: 'Гаджеты'},
-    {id: 1, title: 'Здоровье'},
-    {id: 1, title: 'Автомобиль'},
-    {id: 1, title: 'Ремонт'},
+    {id: 2, title: 'Семья'},
+    {id: 3, title: 'Учеба'},
+    {id: 4, title: 'Отдых'},
+    {id: 5, title: 'Спорт'},
+    {id: 6, title: 'Еда'},
+    {id: 7, title: 'Финансы'},
+    {id: 8, title: 'Гаджеты'},
+    {id: 9, title: 'Здоровье'},
+    {id: 10, title: 'Автомобиль'},
+    {id: 11, title: 'Ремонт'},
   ];
 
   static priorities: Priority[] = [
     {id: 1, title: 'Низкий', color: '#e5e5e5'},
-    {id: 1, title: 'Средний', color: '#e5c231'},
-    {id: 1, title: 'Высокий', color: '#e5691f'},
-    {id: 1, title: 'Очень высокий', color: '#e5050c'},
+    {id: 2, title: 'Средний', color: '#e5c231'},
+    {id: 3, title: 'Высокий', color: '#e5691f'},
+    {id: 4, title: 'Очень высокий', color: '#e5050c'},
   ];
 
   static tasks: Task[] = [
@@ -36,14 +36,14 @@ export class TestData {
       date: new Date('2021-05-25')
     },
     {
-      id: 1,
+      id: 2,
       title: 'Передать отчет',
       priority: TestData.priorities[0],
       completed: false,
       category: TestData.categories[0],
     },
     {
-      id: 1,
+      id: 3,
       title: 'Футбол с сотрудниками',
       priority: TestData.priorities[2],
       completed: false,
@@ -51,7 +51,7 @@ export class TestData {
       date: new Date('2021-05-27')
     },
     {
-      id: 1,
+      id: 4,
       title: 'Сдать анализ',
       priority: TestData.priorities[3],
       completed: false,
@@ -59,7 +59,7 @@ export class TestData {
       date: new Date('2021-05-28')
     },
     {
-      id: 1,
+      id: 5,
       title: 'Самообучение',
       priority: TestData.priorities[1],
       completed: false,
@@ -67,7 +67,7 @@ export class TestData {
       date: new Date('2021-05-29')
     },
     {
-      id: 1,
+      id: 6,
       title: 'Собеседование',
       priority: TestData.priorities[3],
       completed: false,
---
+++ src/app/views/tasks/tasks/tasks.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from "../../../data/service/data-handler.service";
+import {Task} from "../../../model/Task";
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] | undefined;
+
+  constructor(private  dataHandler: DataHandlerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.tasks = this.dataHandler.getTasks()
+    console.log(this.tasks);
+  }
+
+}
